c2cf54c1475265c5e06a5c054fd41fbf846a6335
Wes Higbee <wes.mcclure@gmail.com>
2015-09-23 20:49:06 -0400

    Add explicit dependencies to karma plugins to package.json and also get firefox testing working in addition to Chrome.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 
     frameworks: ["jasmine"],
 
-    browsers: ["Chrome"],
+    browsers: ["Chrome", "Firefox"],
 
     plugins: [
       "karma-chrome-launcher",

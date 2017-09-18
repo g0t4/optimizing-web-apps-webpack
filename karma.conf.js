33ca021862602c6785b59fd26e1c15cc013a0b80
Wes Higbee <wes.mcclure@gmail.com>
2017-09-18 12:19:14 -0400

    Disable firefox tests

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,10 @@ module.exports = function (config) {
 
     frameworks: ["jasmine"],
 
-    browsers: ["Chrome", "Firefox"],
+    browsers: [
+      "Chrome",
+      //"Firefox"
+    ],
 
     plugins: [
       "karma-chrome-launcher",

a77d57ba2936ca2482a107f2e445c65210052d0d
Wes Higbee <wes.mcclure@gmail.com>
2017-10-16 21:56:46 -0400

    Import remaining three app.js dependencies

--- app/app.js
+++ app/app.js
@@ -1,3 +1,6 @@
 import "./klondike/scoring.js";
+import "./klondike/klondike.js";
+import "./klondike/board.js";
+import "./klondike/game.js";
 
 angular.module("solitaire", ["klondike", "ngDraggable"]);

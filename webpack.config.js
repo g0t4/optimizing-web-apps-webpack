70cbb379ecba52c4256e510dff65d6a478836e9b
Wes Higbee <wes.mcclure@gmail.com>
2017-10-23 21:37:25 -0400

    Get HMR setup minus replacement code in the client

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './app/app.js',
   output: {
     path: path.resolve(__dirname, 'app/dist'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
+    publicPath: '/dist/',
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'app'),
     publicPath: '/dist/',
-    watchContentBase: true
-  }
+    watchContentBase: false,
+    hotOnly: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };

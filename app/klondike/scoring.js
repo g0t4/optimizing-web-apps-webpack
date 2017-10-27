92490973785701609117f7aa8c1d470d111ee1cd
Wes Higbee <wes.mcclure@gmail.com>
2017-10-26 23:15:00 -0400

    Convert scoring to class

--- app/klondike/scoring.js
+++ app/klondike/scoring.js
@@ -1,32 +1,37 @@
-function Scoring() {
-  "use strict";
+class Scoring {
 
-  this.score = 0;
-
-  this.newGame = function () {
+  constructor() {
     this.score = 0;
-  };
-  this.tableauCardTurnedUp = function () {
-    this.score += 5;
-  };
-  this.dropped = function (source, destionation) {
-    this.score += scoreForMoving(source, destionation) || 0;
-  };
-  this.wasteRecycled = function () {
-    this.score = Math.max(this.score - 100, 0);
-  };
+  }
 
-  function scoreForMoving(source, destionation) {
-    if (destionation.name === "TableauPile") {
-      if (source.name === "FoundationPile") {
-        return -15;
-      }
-      return 5;
+  newGame() {
+    this.score = 0;
+  }
+
+  tableauCardTurnedUp() {
+    this.score += 5;
+  }
+
+  dropped(source, destination) {
+    this.score += scoreForMoving(source, destination) || 0;
+  }
+
+  wasteRecycled() {
+    this.score = Math.max(this.score - 100, 0);
+  }
+
+}
+
+function scoreForMoving(source, destination) {
+  if (destination.name === "TableauPile") {
+    if (source.name === "FoundationPile") {
+      return -15;
     }
-    if (destionation.name === "FoundationPile") {
-      if (source.name === "TableauPile" || source.name === "WastePile") {
-        return 10;
-      }
+    return 5;
+  }
+  if (destination.name === "FoundationPile") {
+    if (source.name === "TableauPile" || source.name === "WastePile") {
+      return 10;
     }
   }
 }
@@ -44,10 +49,10 @@ if (module.hot) {
   const doc = angular.element(document);
   const injector = doc.injector();
   if (injector) {
-    const actualService = injector.get("scoring");
-    const newScoringService = new Scoring();
+    const actualService = Object.getPrototypeOf(injector.get("scoring"));
+    const newScoringService = Object.getPrototypeOf(new Scoring());
     // note: just replaces functions
-    Object.keys(actualService)
+    Object.getOwnPropertyNames(actualService)
       .filter(key => typeof actualService[key] === "function")
       .forEach(key => actualService[key] = newScoringService[key]);
     doc.find('html').scope().$apply();
@@ -56,4 +61,4 @@ if (module.hot) {
 }
 
 angular.module("klondike.scoring", [])
-  .service("scoring", [Scoring]);
+  .factory("scoring", () => new Scoring());

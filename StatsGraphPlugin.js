9e2390d3ee02973cc3fa98883a0720fe771b9b3a
Wes Higbee <wes.mcclure@gmail.com>
2017-11-14 17:55:19 -0500

    not shown in course - Add more verbosity (if enabled) in StatsGraphPlugin

--- StatsGraphPlugin.js
+++ StatsGraphPlugin.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const {exec} = require('child_process');
 
+const messagePrefix = '[StatsGraphPlugin]';
+
 class StatsGraphPlugin {
 
   constructor(options) {
@@ -17,36 +19,54 @@ class StatsGraphPlugin {
   // todo when the new hook syntax arrives (TBD) this plugin will need migrated
   apply(compiler) {
     compiler.plugin('done', stats => {
-      writeStatsJson(stats, compiler);
-      runWebpackStatsGraph(this.options);
+      this.writeStatsJson(stats, compiler);
+      this.runWebpackStatsGraph();
     });
   }
 
-}
+  writeStatsJson(stats, compiler) {
+    this.info(`writing stats.json`);
+    const toJson = stats.toJson(compiler.options.output);
+    const statsJson = JSON.stringify(toJson, null, 2);
+    fs.writeFileSync('stats.json', statsJson);
+  }
 
-function writeStatsJson(stats, compiler) {
-  const toJson = stats.toJson(compiler.options.output);
-  const statsJson = JSON.stringify(toJson, null, 2);
-  fs.writeFileSync('stats.json', statsJson);
-}
+  runWebpackStatsGraph() {
+    // this plugin expects to find webpack-stats-graph command globally:
+    // npm install -g webpack-stats-graph
+    this.info(`executing webpack-stats-graph`);
+    exec('webpack-stats-graph', (error, stdout, stderr) => {
 
-function runWebpackStatsGraph(options) {
-  // this plugin expects to find webpack-stats-graph command globally:
-  // npm install -g webpack-stats-graph
-  exec('webpack-stats-graph', (error, stdout, stderr) => {
+      if (error) {
+        console.group(`${messagePrefix} webpack-stats-graph failed`);
+        console.error(error);
+      }
+      else {
+        console.group(`${messagePrefix} webpack-stats-graph done`);
+      }
 
-    const messagePrefix = '[StatsGraphPlugin]: webpack-stats-graph';
+      if (error || (stdout && this.options.verbose)) {
+        console.group('stdout:');
+        console.log(stdout);
+        console.groupEnd();
+      }
 
-    if (error)
-      console.error(`${messagePrefix} failed:\n${error}`);
+      if (stderr) {
+        console.group('stderr:');
+        console.error(stderr);
+        console.groupEnd();
+      }
 
-    if (error || (stdout && options.verbose))
-      console.log(`${messagePrefix} stdout:\n${stdout}`);
+      console.groupEnd();
 
-    if (stderr)
-      console.log(`${messagePrefix} stderr:\n${stderr}`);
+    });
+  }
 
-  });
+  info(message) {
+    if (this.options.verbose)
+      console.log(`${messagePrefix} ${message}`);
+  }
+
 }
 
 module.exports = StatsGraphPlugin;

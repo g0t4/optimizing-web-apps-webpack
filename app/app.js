d91a90fa6a6e81394b7be1b40026acdb3b66d21b
Wes Higbee <wes.mcclure@gmail.com>
2017-11-18 11:54:31 -0500

    Only polyfill the Promise type from core-js, that's all we need (or so it seems)

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 //import "@babel/polyfill";
-import "core-js/shim";
+//import "core-js/shim";
+import "core-js/es6/promise";
 import "./klondike/scoring.js";
 import "./klondike/klondike.js";
 import "./klondike/board.js";

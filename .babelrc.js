2595406f13d48b081f8ba72e69a0390949045695
Wes Higbee <wes.mcclure@gmail.com>
2017-11-20 12:56:35 -0500

    Changing browsers changes polyfills

--- .babelrc.js
+++ .babelrc.js
@@ -4,7 +4,7 @@ module.exports = {
       debug: true,
       modules: false,
       targets: {
-        browsers: ['> 1%']
+        browsers: ['> 1%', 'not IE < 12']
       },
       useBuiltIns: 'entry'
     }]

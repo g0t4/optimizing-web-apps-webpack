14a6269ba1b14f101aec59b14c51e4536e660540
Wes Higbee <wes.mcclure@gmail.com>
2017-11-21 17:48:53 -0500

    Add tee-loader and configure it to run after the babel-loader.

--- babelLoader.js
+++ babelLoader.js
@@ -1,13 +1,21 @@
+const path = require('path');
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-        }
+        use: [
+          'tee-loader',
+          'babel-loader'
+        ]
       }
     ]
   },
+  resolveLoader: {
+    alias: {
+      'tee-loader': path.resolve(__dirname, 'tee-loader.js')
+    }
+  }
 };
---
+++ tee-loader.js
@@ -0,0 +1,4 @@
+module.exports = function (source) {
+  console.log('[tee-loader]');
+  return source;
+};

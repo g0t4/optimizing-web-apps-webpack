d9dfa016385b044eb064d114952aa8222eea6235
Wes Higbee <wes.mcclure@gmail.com>
2017-11-21 22:11:03 -0500

    Example of inline loaders (reverted from commit ca63733e2d5d04c879be23bef0710562244aa9ad)

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-import "!!tee-loader?label=inline-after!babel-loader!tee-loader?label=inline-before!./klondike/scoring.js";
+import "./klondike/scoring.js";
 import "./klondike/klondike.js";
 import "./klondike/board.js";
 import "./klondike/game.js";

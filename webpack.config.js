505c52896e8b0078e50d2726ed760ac492cde05f
Wes Higbee <wes.mcclure@gmail.com>
2017-12-15 15:31:24 -0500

    Clean output folder before bundling

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const StatsGraphPlugin = require('./StatsGraphPlugin');
 const babelLoader = require('./babelLoader');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 
 module.exports = function (env) {
 
@@ -17,6 +19,7 @@ module.exports = function (env) {
       publicPath: '/dist/',
     },
     plugins: [
+      new CleanWebpackPlugin(['app/dist']),
       new webpack.DefinePlugin({
         ENV_IS_DEVELOPMENT: isDevelopment,
         ENV_IS: JSON.stringify(env),

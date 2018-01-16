adef550b7eeef879a65dead2cab07fbd11afb633
Wes Higbee <wes.mcclure@gmail.com>
2018-01-16 18:41:23 -0500

    Enable source maps just to demo the value of source maps in debugging a runtime error

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = function (env) {
 
   const baseConfig = {
     entry: './app/app.js',
+    devtool: 'source-map',
     output: {
       path: path.resolve(__dirname, 'app/dist'),
       filename: 'app.bundle.js',

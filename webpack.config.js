8e67fa99cfdf8dadcfa3cb7a9ec87b19776bd58e
Wes Higbee <wes.mcclure@gmail.com>
2018-01-16 21:18:46 -0500

    devtool: "eval"

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = function (env) {
 
   const baseConfig = {
     entry: './app/app.js',
-    devtool: 'source-map',
+    devtool: 'eval',
     output: {
       path: path.resolve(__dirname, 'app/dist'),
       filename: 'app.bundle.js',
